203e8f13ca22465d7b6ca2909b27011ed5d45293
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-01-27 16:59:00 +0100

    1.03

--- js/app.jsx
+++ js/app.jsx
@@ -23,7 +23,11 @@ class App extends React.Component {
             </section>
             <section className="articles">
                 <div className="container">
-
+                    <h1>2017</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut delectus, eius in libero velit veniam. Aperiam architecto, distinctio, est et expedita fuga harum id incidunt minima odio pariatur qui voluptas?
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magnam quasi reiciendis reprehenderit vel voluptatibus. Ad aspernatur autem, dolorem, explicabo facilis libero non officia porro praesentium recusandae, sapiente sequi ullam.
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi at aut consectetur consequatur dicta dolore earum fuga illo impedit modi, placeat praesentium quasi, quo, rem saepe sit sunt suscipit tenetur.
+                    </p>
                 </div>
             </section>
             <section className="main">
