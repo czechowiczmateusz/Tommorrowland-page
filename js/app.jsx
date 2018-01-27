b8c91a6c86c86ef667d4e78997b3854a5433c762
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-01-27 16:44:28 +0100

    1.02

--- js/app.jsx
+++ js/app.jsx
@@ -7,14 +7,25 @@ class App extends React.Component {
         return <div>
             <header>
                 <div className="container">
-                    <h1>Tommorrowland</h1>
-                    <ul>
-                        <li>About</li>
+                    <ul className="left">
+                        <img src="https://images.contentful.com/9uj5p2yo1ox0/545FhqlS40mYEegoEOYIcu/487935385221aaf84f25649d9b17c407/TML_ICON_BLACK_OUTLINE.png" />
+                        <li>Tommorrowland</li>
+                    </ul>
+                    <ul className="right">
+                        <li>Tickets</li>
                         <li>FAQ</li>
                         <li>Contact</li>
                     </ul>
                 </div>
             </header>
+            <section className="main">
+                <div className="container"></div>
+            </section>
+            <section className="articles">
+                <div className="container">
+
+                </div>
+            </section>
             <section className="main">
                 <div className="container"></div>
             </section>
