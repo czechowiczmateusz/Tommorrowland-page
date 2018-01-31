55b75bedc9833c01e6c5423b112c3d8f34cd0995
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-01-31 14:27:15 +0100

    4.0

--- js/components/Footer/Footer.jsx
+++ js/components/Footer/Footer.jsx
@@ -5,10 +5,18 @@ class Footer extends React.Component {
         return (
             <footer>
                 <div className="container">
+                    <form>
+                        <h2>Contact us</h2>
+                        <input type="text" placeholder="Name"/>
+                        <input type="email" placeholder="Email"/>
+                        <input type="text" placeholder="Phone"/>
+                        <textarea name="Message" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                        <input type="submit" value="Submit" disabled/>
+                    </form>
                     <ul>
                         <li><i className="fa fa-facebook" aria-hidden="true"></i>Facebook</li>
-                        <li><i className="fa fa-instagram" aria-hidden="true"></i>Twitter</li>
-                        <li><i className="fa fa-twitter" aria-hidden="true"></i>Instagram</li>
+                        <li><i className="fa fa-instagram" aria-hidden="true"></i>Instagram</li>
+                        <li><i className="fa fa-twitter" aria-hidden="true"></i>Twitter</li>
                     </ul>
                 </div>
             </footer>
