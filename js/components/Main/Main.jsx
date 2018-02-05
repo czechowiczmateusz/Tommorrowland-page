44294f9b3a97aac8e015efda1c37e8532daf02a4
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-02-05 10:25:34 +0100

    4.1

--- js/components/Main/Main.jsx
+++ js/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Main extends React.Component {
     render(){
         return (
-            <section className="main"></section>
+            <main></main>
         )
     }
 }
