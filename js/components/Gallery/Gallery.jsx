578ae4404c107e8b13c62e2f3d87a8c40cdbcf15
czechowiczmateusz <matic1.czechowicz@gmail.com>
2018-05-08 13:34:25 +0200

    5.01

--- js/components/Gallery/Gallery.jsx
+++ js/components/Gallery/Gallery.jsx
@@ -5,9 +5,9 @@ class Gallery extends React.Component {
         return (
             <section className="slider">
                 <div className="container">
-                    <img src="https://starlightpr1.com/upload/blog_image/1516256012_tomorrowland-belgium-atmosphere-2016-billboard-1548.jpg"/>
-                    <img src="https://i.pinimg.com/564x/1d/4c/fd/1d4cfdc27b5db9de7c006d59ab7cb101--tomorrowland--tomorrowland-belgium.jpg"/>
-                    <img src="https://i.pinimg.com/originals/9e/60/08/9e600838a00c4c7d863a3fc47751025d.jpg"/>
+                    <img src={require('./../../../styles/images/gallery1.jpg')}/>
+                    <img src={require('./../../../styles/images/gallery2.jpg')}/>
+                    <img src={require('./../../../styles/images/gallery3.jpg')}/>
                 </div>
             </section>
         );
